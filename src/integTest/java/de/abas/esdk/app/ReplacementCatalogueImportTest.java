5ca2d3440d7ce853d82b10bc8934b8c09406937d
jazz <jasmin.borsig@gmail.com>
2019-10-18 16:39:44 +0200

    ESDK-1683 test infosystem row displaying

--- src/integTest/java/de/abas/esdk/app/ReplacementCatalogueImportTest.java
+++ src/integTest/java/de/abas/esdk/app/ReplacementCatalogueImportTest.java
@@ -7,8 +7,12 @@ import de.abas.esdk.test.util.ServerSideErrorMessageException;
 import org.junit.After;
 import org.junit.Test;
 
+import java.math.BigDecimal;
+
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.core.StringContains.containsString;
+import static org.hamcrest.number.BigDecimalCloseTo.closeTo;
 import static org.junit.Assert.fail;
 
 public class ReplacementCatalogueImportTest extends EsdkIntegTest {
@@ -46,6 +50,30 @@ public class ReplacementCatalogueImportTest extends EsdkIntegTest {
 		} catch (ServerSideErrorMessageException e) {
 			assertThat(e.getMessage(), containsString("File does not exist. Please enter a valid import file location."));
 		}
+	}
+
+	@Test
+	public void canDisplayReplacementCatalogueItems() {
+		replacementCatalogue.setFile("ow1/REPLC1.TEST.CSV");
+		replacementCatalogue.invokeStart();
+		ReplacementCatalogue.Table table = replacementCatalogue.table();
+		assertThat(table.getRowCount(), is(3));
+		validateRow(table.getRow(1), "72645", "Mutter, selbstsichernd", "MU34", 3.12);
+		validateRow(table.getRow(2), "92384", "Schraube 60mm, Senkkopf", "SCREW", 5.42);
+		validateRow(table.getRow(3), "83474", "Blech, titan", "TITAN", 7.98);
+	}
+
+	private void validateRow(final ReplacementCatalogue.Row row, final boolean toBeImported, final String icon, final String productNo, final String descr, final String matchcode, final double price) {
+		assertThat(row.getImport(), is(toBeImported));
+		assertThat(row.getTransfericon(), is(icon));
+		assertThat(row.getProductno(), is(productNo));
+		assertThat(row.getDescr(), is(descr));
+		assertThat(row.getMatchcode(), is(matchcode));
+		assertThat(row.getPrice(), is(closeTo(new BigDecimal(price), new BigDecimal(0.001))));
+	}
+
+	private void validateRow(final ReplacementCatalogue.Row row, final String productNo, final String descr, final String matchcode, final double price) {
+		validateRow(row, false, "", productNo, descr, matchcode, price);
 	}
 
 	@After
